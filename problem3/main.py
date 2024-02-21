aa783716dfcf01dfc7ab06f15626ccb8e7511f7a
awinardi1004 <awinardi1004@gmail.com>
2024-02-21 10:50:43 +0700

    menambahkan tugas part5 problem 3

--- problem3/main.py
+++ problem3/main.py
@@ -1,6 +1,14 @@
 def join_array_remove_duplicate(arrayA, arrayB):
-    # your code here
-    return []
+    result_array = arrayA.copy()
+    for x in arrayB:
+        if x not in result_array:
+            result_array.append(x)
+        
+    unique_array = []
+    for i in result_array:
+        if i not in unique_array:
+            unique_array.append(i)
+    return unique_array
 
 if __name__ == '__main__':
     # Test cases
